cc040e49354421c1119197b79a2105aa51fc80a7
orelalon <orela231089@gmail.com>
2024-05-28 14:11:29 +0300

    cosmetic to stairs

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -12,7 +12,8 @@ const StairTransition = () => {
     <><AnimatePresence mode="wait">
       <div key={pathname}>
 
-        <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+        <div className="h-screen w-screen fixed top-0 left-0
+         right-0 pointer-events-none z-40 flex">
           <Stairs/>
         </div>
       </div>
--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -5,7 +5,7 @@ const stairAnimation={
         top:'0%',
     },
     animate:{
-                top:'0%',
+                top:'100%',
 
     },
     exit:{
@@ -21,8 +21,8 @@ const reverseIndex = (index)=>{
 const Stairs = () => {
   return (
     <>
-    {[...Array(6).map((_,index)=>{
-<motion.div
+    {[...Array(6)].map((_,index)=>{
+        return(<motion.div
 key= {index}
 variants={stairAnimation}
 initial='initial'
@@ -34,9 +34,10 @@ transition={{
     delay: reverseIndex(index)*0.1,
 }}
 className="h-full w-full bg-white relative"
-/>
+/>)
 
-    })]}
+
+    })}
     </>
   )
 }
