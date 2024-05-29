f3d08c963b55a8be8e563d162cbace1090d4f35b
orelalon <orela231089@gmail.com>
2024-05-29 15:05:30 +0300

    creating socials section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
 import { Button } from '@/components/ui/button'
 import {FiDownload} from 'react-icons/fi'
 
+
+// components
+import Social from '@/components/Social'
+
 const Home = () => {
   return (
     <section className='h-full'>
@@ -12,13 +16,24 @@ const Home = () => {
           <span className='text-xl'>Web Developer</span>
           <h1 className='h1 mb-6'> Hello I'm <br /><span className='text-accent'>Orel Alon</span></h1>
         <p className='max-w-[500px] mb-9 text-white/80'>I need to add somthing here</p>
+       
+       <div className='flex flex-col xl:flex-row items-center gap-8'>
+        <Button variant='outline' size='lg' className=' flex items-center gap-2'>
+          <span>Download CV</span>
+          <FiDownload className='text-xl'/>
+          </Button>
+               <div className='mb-8 xl:mb-0'><Social/></div> 
+       </div>
+  
+      </div>
+       
+       </div>
+       
         </div>
         {/* photo */}
         <div> photo</div>
         <div> text</div>
-</div>
-        homepage
-      </div>
+
     </section>
   )
 }
