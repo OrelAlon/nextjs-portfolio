67b47cdf20b5d33570f4e33a57e8503e20464acb
orelalon <orela231089@gmail.com>
2024-05-30 15:00:14 +0300

    cosmetic to mage

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -2,24 +2,31 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import orel from './../components/img/orel.png'
 
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-      <motion.div>
-        <div className="relative w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
+      <motion.div initial={{opacity:0}}animate={{opacity:1,transition:{delay:2,duration:0,ease:'easeIn'} }}>
+      <motion.div initial={{opacity:0}}animate={{opacity:1,transition:{delay:2.4,duration:0.4,ease:'easeInOut'} }}>
+       
+       
+        <div className=" w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
           <Image 
-            src='/me-b.png' 
+            src='/../public/next.svg' 
             priority 
             quality={100} 
             fill 
-            alt='Description of image' 
+            alt='f image' 
             className="object-contain"
           />
           photo
-          <img src="/mee.jpeg" alt="Description of image" />
+          <img src={orel} alt=" image" />
         </div>
       </motion.div>
+      </motion.div>
+                <img src={orel} alt=" image" />
+
     </div>
   );
 };
