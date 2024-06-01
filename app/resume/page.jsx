3894281300b3a396ab0e01841174a7d9056352f3
orelalon <orela231089@gmail.com>
2024-06-01 11:17:13 +0300

    fix prettier

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,31 @@
+'use client';
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
+
+// about data
+const about = {
+  title: 'About Me',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+  info: [
+    { fieldName: 'Name', fieldValue: 'Orel Alon' },
+    { fieldName: 'Phone', fieldValue: '052-6707895' },
+    { fieldName: 'Email', fieldValue: 'Orel.Alon.dev@gmail.vom' },
+    { fieldName: 'Name', fieldValue: 'Orel Alon' },
+  ],
+};
+
+const experience = {};
 
 const Resume = () => {
-  return (
-    <div>resume page</div>
-  )
-}
+  return <div>resume page</div>;
+};
 
-export default Resume
+export default Resume;
