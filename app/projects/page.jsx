8174616bbd24ce2e5ee62477cb3ab0d9f652d3af
orelalon <orela231089@gmail.com>
2024-06-05 23:02:31 +0300

    create WorkSliderBtns component

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,6 +16,7 @@ import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import WorkSliderBtns from '@/components/WorkSliderBtns';
 
 const projects = [
   {
@@ -60,7 +61,10 @@ const Projects = () => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+      }}
       className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'
     >
       <div className='container mx-auto'>
@@ -129,7 +133,7 @@ const Projects = () => {
                 return (
                   <SwiperSlide key={index} className='w-full'>
                     <div className='h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
-                      <div></div>
+                      <div className='absolute top-0 bottom-0 w-full h-full bg-black/10 z-10'></div>
                       <div className='relative w-full h-full '>
                         <Image
                           src={project.image}
@@ -139,10 +143,10 @@ const Projects = () => {
                         ></Image>
                       </div>
                     </div>
-                    slide
                   </SwiperSlide>
                 );
               })}
+              <WorkSliderBtns />
             </Swiper>
           </div>
         </div>
