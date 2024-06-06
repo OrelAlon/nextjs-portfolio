e0aef89439a8fbc822fa4b71a8c8462182a28097
orelalon <orela231089@gmail.com>
2024-06-06 21:13:35 +0300

    cosmetic to contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,7 +51,24 @@ const Contact = () => {
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           <div className='xl:h-[54%] order-2 xl:order-none'>
             <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
-              <h3>Let's work together</h3>
+              <h3 className='text-4xl text-accent'>Let's work together</h3>
+              <p className='text-white/60'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit est nesciunt et magni at. Saepe tenetur aliquid
+                voluptas quis consectetur illo, aspernatur iusto eos, officia
+                quos corrupti facere in. Laboriosam.
+              </p>
+              <div className='grid grid-cols-1 md:grid-cols gap-6'>
+                <Input type='firstname' placeholder='Firstname' />
+                <Input type='lastname' placeholder='Lastname' />
+                <Input type='email' placeholder='Email asddress' />
+                <Input type='phone' placeholder='Phone number' />
+              </div>
+              <Select>
+                <SelectTrigger className='w-full'>
+                  <SelectValue placeholder='Select a service'></SelectValue>
+                </SelectTrigger>
+              </Select>
             </form>
           </div>
           <div className='flex-1 flex items-center xl-justify-end order-1 xl:order-none mb-8 xl:mb-0'>
