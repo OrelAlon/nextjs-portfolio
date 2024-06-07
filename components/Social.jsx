b583c6fa066615cb50717201107fb6010cd9fda3
orelalon <orela231089@gmail.com>
2024-06-07 08:25:51 +0300

    fix social component

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,27 +1,31 @@
-import Link from 'next/link'
-import {FaGithub,FaLinkedinIn, FaPhone, FaMailBulk} from 'react-icons/fa'
+import Link from 'next/link';
+import { FaGithub, FaLinkedinIn, FaPhone, FaMailBulk } from 'react-icons/fa';
 
-const socials=[
-        {icon:<FaGithub/>, path:''},
-        {icon:<FaLinkedinIn/>, path:''},
-        {icon:<FaPhone/>, path:''},
-        {icon:<FaMailBulk/>, path:''},
+const socials = [
+  { icon: <FaGithub />, path: 'https://github.com/OrelAlon' },
+  { icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/orel-alon/' },
+  { icon: <FaPhone />, path: 'tel:0526707895' },
+  { icon: <FaMailBulk />, path: 'mailto:orel.alon.dev@gmail.com' },
+];
 
-]
-
-const Social = ({containerStyles,iconStyles}) => {
+const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-        
-       {socials.map((item,index)=>{
-            return(
-                <Link key={index} href={item.path} className={iconStyles}>
-                    {item.icon}
-                </Link>
-            )
-       })}
+      {socials.map((item, index) => {
+        return (
+          <Link
+            target='_blank'
+            rel='noopener noreferrer'
+            key={index}
+            href={item.path}
+            className={iconStyles}
+          >
+            {item.icon}
+          </Link>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
